8692084b8d139f6da52d773b23c0b6c71c531f74
Alex Roland <amr0098@mix.wvu.edu>
2024-03-07 21:28:02 -0500

    Attempt to resolve errors in jammer when running through framework.

--- modules/Jammer.py
+++ modules/Jammer.py
@@ -1,32 +1,18 @@
 #!/usr/bin/env python3
-###################################################################################
-# Importing Libraries
-###################################################################################
 
 import time
 import yaml
+import gnuradio
 from gnuradio import gr
-from gnuradio import blocks
 from gnuradio import analog
-from gnuradio import audio
-from gnuradio import fft
 from gnuradio.fft import window
-from gnuradio import filter
 from gnuradio.filter import firdes
 from statistics import mean
 import osmosdr
-import sys
-import numpy as np
 from random import randint
 
-###################################################################################
-# Jamming Channel Function
-###################################################################################
-
 def jam(freq, power, delay=1):
-    ###############################################################################
-    # Variables
-    ###############################################################################
+    
     print(f"\nThe frequency currently jammed is: {freq / (10e5)}MHz")
     samp_rate = 20e6  # Sample Rate
     sdr_bandwidth = 20e6  # Hackrf SDR Bandwidth
@@ -34,15 +20,10 @@ def jam(freq, power, delay=1):
 
     tb = gr.top_block()
 
-    ##############################################################################
-    # waveform selection
-    ##############################################################################
-
     source = analog.noise_source_c(analog.GR_GAUSSIAN, 1, 1)
 
-    #############################################################################
-    # Hackrf Trasmitter options
-    #############################################################################
+    #HackRF parameters are set below. These are specific to the hackrf and are set with osmosdr.sink
+    #all other supplied values are tuned for the device.
 
     freq_mod = analog.frequency_modulator_fc(1)
     osmosdr_sink = osmosdr.sink(
@@ -58,10 +39,6 @@ def jam(freq, power, delay=1):
     osmosdr_sink.set_antenna('', 0)
     osmosdr_sink.set_bandwidth(sdr_bandwidth, 0)
 
-    ##############################################################################
-    # Connecting Blocks
-    ##############################################################################
-
     tb.connect(source, osmosdr_sink)
 
     tb.start()
@@ -69,23 +46,7 @@ def jam(freq, power, delay=1):
     tb.stop()
     tb.wait()
 
-
-##################################################################################
-# Set Frequency
-##################################################################################
-
-def set_frequency(channel, ch_dist):
-    if channel == 1:
-        freq = init_freq
-    else:
-        freq = init_freq + (channel - 1) * ch_dist
-
-    return freq
-
-
-##################################################################################
-# Set RF Gains
-##################################################################################
+#Gain set through signal power calculation.
 
 def set_gains(power):
     if -40 <= power <= 5:
@@ -104,9 +65,7 @@ def set_gains(power):
 
     return RF_gain, IF_gain
 
-##################################################################################
-# Main Function
-##################################################################################
+#Main calling previous functions
 
 if __name__ == "__main__":
     
