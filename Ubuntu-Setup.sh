cda88bf35cb4c07bdb3128871c41a53b59374113
Ubuntu <ubuntu@ubuntu-2204.linuxvmimages.local>
2024-03-23 18:38:09 -0400

    Removed requirements.txt, added Ubuntu Setup Script

---
+++ Ubuntu-Setup.sh
@@ -0,0 +1,9 @@
+sudo apt-get update -y
+sudo apt install git -y
+sudo apt-get install python3.11 -y
+sudo apt install pip -y
+sudo apt-get install python3-tk -y
+sudo pip3 install sv_ttk
+sudo apt-get install -y hackrf libhackrf-dev libhackrf0
+sudo apt install gnuradio -y
+sudo apt install gr-osmosdr -y
